1f963b050c1e96dba2751a7a020f40b9919ac598
SriharshaMandru <mandrusriharsha@gmail.com>
2024-06-08 13:12:00 +0530

    modifications are made in login files and fileupload files

---
+++ index.js
@@ -0,0 +1,67 @@
+const fileList = document.querySelector(".file-list");
+const fileBrowseButton = document.querySelector(".file-browse-button");
+const fileBrowseInput = document.querySelector(".file-browse-input");
+const fileUploadBox = document.querySelector(".file-upload-box");
+
+const createFileHTML = (file, index) => {
+    const {name,size}=file;
+    const extension = name.split(".").pop();
+
+    return ` <li class="file-item">
+                <div class="file-extension">${extension}</div>
+                <div class="file-content-wrapper">
+                    <div class="file-content">
+                        <div class="file-details">
+                            <h5 class="file-name">${name}</h5>
+                            <div class="file-info">
+                                <small class="file-size">4 mb/${size}</small>
+                                <small class="file-divider">.</small>  
+                                <small class="file-status">Uploading....</small>
+                            </div>
+                        </div>
+                        <button class="cancel-button">
+                            <i class="bx bx-x"></i>
+                        </button>
+                    </div>
+                    <div class="file-progress-bar">
+                        <div class="file-progress"></div>
+                    </div>
+                </div>
+            </li>`
+}
+
+const handleFileUploading = (file, index) =>{
+    const xhr = new XMLHttpRequest();
+    const formData = new FormData();
+    formData.append("file",file);
+
+    xhr.open("POST","API_ENDPOINT",true);
+    xhr.send(formData);
+}
+
+const handleSelectedFiles = ([...files]) => {
+    if(files.length === 0) return;
+
+    files.forEach((file, index) => {
+          const fileItemHTML = createFileHTML(file, index); 
+          fileList.insertAdjacentHTML("afterbegin",fileItemHTML);
+          
+          handleFileUploading(file, index);
+    });
+}
+
+fileUploadBox.addEventListener("drop",(e) => {
+    e.preventDefault();
+    handleSelectedFiles(e.dataTransfer.files);
+    fileUploadBox.classList.add("active");
+    fileUploadBox.querySelector(".file-instruction").innerText = "Release to upload or";
+});
+fileUploadBox.addEventListener("dragleave",(e) => {
+    e.preventDefault();
+    fileUploadBox.classList.remove("active");
+    fileUploadBox.querySelector(".file-instruction").innerText = "Drag files here";
+});
+
+
+fileBrowseInput.addEventListener("change",(e) => handleSelectedFiles(e.target.files));
+fileBrowseButton.addEventListener("click",()=>fileBrowseInput.click());
--- login.js
+++ login.js
@@ -1,9 +1,17 @@
-var input1= document.getElementById("input1");
-var input2= document.getElementById("input2");
-var login= document.getElementById("login");
-input1.addEventListener('keypress',function(e){
-if(e.keyCode===13)
-    {
-        input2.style.display="block";
+var input1 = document.getElementById("input1");
+var input2 = document.getElementById("input2");
+var passwordInput = document.getElementById("password");
+var loginButton = document.getElementById("loginButton");
+
+input1.addEventListener('keypress', function(e) {
+    if (e.keyCode === 13) {
+        input2.style.display = "block";
     }
 });
+
+/*loginButton.addEventListener("click", function() {
+    var password = passwordInput.value;
+    if (password === "") {
+
+    }
+});*/
