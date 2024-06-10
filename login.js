ca903d603f697750b88082841bc3ccb8a1ce0717
SriharshaMandru <mandrusriharsha@gmail.com>
2024-06-10 15:02:08 +0530

    modified files are added

--- login.js
+++ login.js
@@ -1,17 +1,12 @@
 var input1 = document.getElementById("input1");
 var input2 = document.getElementById("input2");
-var passwordInput = document.getElementById("password");
-var loginButton = document.getElementById("loginButton");
 
-input1.addEventListener('keypress', function(e) {
-    if (e.keyCode === 13) {
-        input2.style.display = "block";
+
+input1.addEventListener("keypress",function(e){
+    if(e.keyCode === 13)
+    {
+        e.preventDefault();
+         input2.style.display = "block";
     }
 });
-
-/*loginButton.addEventListener("click", function() {
-    var password = passwordInput.value;
-    if (password === "") {
-
-    }
-});*/
+  
